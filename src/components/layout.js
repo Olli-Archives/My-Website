bcc76aceaee0bfd81d8a59a5e3971a465e5ad595
ollinieminen <otn987@gmail.com>
2019-05-18 11:34:53 -0700

    start styling homepage

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,19 +18,18 @@ const Layout = ({ children }) => (
       query SiteTitleQuery {
         site {
           siteMetadata {
-            title
+            title,
           }
         }
       }
     `}
-    render={data => (
+    render={tomato => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Header siteTitle={tomato.site.siteMetadata.title} />
         <div
           style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
+            margin: `0`,
+            
             paddingTop: 0,
           }}
         >
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,122 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, StaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Img from 'gatsby-image';
+
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+
+  <StaticQuery query={graphql`
+  {
+    faceShot: file(relativePath: {eq:"OllieNieminen.jpg"}){
+          childImageSharp{
+            fluid(maxWidth:1600){
+              ...GatsbyImageSharpFluid
+            }
+          }
+    }
+  }
+    `} render={(data) => {
+      console.log('data', data.faceShot);
+      return (
+        <Layout>
+          {/* image and profile container*/}
+          <div style={{ display: 'flex', textAlign: 'center' }}>
+            <Img fluid={data.faceShot.childImageSharp.fluid} />
+
+            <div style={{ backgroundColor: '', alignSelf: 'center', justifyContent: 'center' }}>
+              <p style={
+                {
+                  margin: ' 0 0 0 30px',
+                  padding: '20px 0 0 0 ',
+                  textAlign: 'left',
+                  fontSize: '50px'
+                }}>"</p>
+              <p style={{ fontSize: '30px', margin: '30px' }}>
+                Full Stack Software Developer with a
+                passion for diving under the hood to get a
+                deeper understanding on new technology.
+                When I'm not helping my friends fix something
+                 they broke, you can find me enjoying the outdoors.
+              </p>
+              <p style={{
+                textAlign: 'right',
+                fontSize: '50px',
+                marginRight: '30px',
+                padding: '0 0 30px 0'
+              }}>"</p>
+            </div>
+          </div>
+          {/* stack container */}
+          <section style={{
+            display: 'flex',
+            height: '600px',
+            backgroundColor: 'silver'
+
+          }}>
+            <div className='tab' style={
+              {
+                width: '125px',
+                backgroundColor: 'blue',
+                writingMode: 'vertical-rl',
+                textOrientation: 'upright',
+                textAlign: 'center',
+                display: 'flex'
+
+
+              }
+            }>
+              <h2 style={{
+                alignSelf: 'center',
+                margin: 'auto',
+                fontSize: '50px'
+              }}>MY STACK</h2>
+            </div>
+            <section className='stack' style={
+              {
+                display: 'flex',
+                flexGrow: '1'
+
+              }
+            }>
+              <div className='front-end-stack' style={{
+                backgroundColor: 'green',
+                flexGrow: 1,
+                textAlign: 'center'
+
+              }}>
+                <h2 style={{
+                  marginTop: '30px',
+                  fontSize: '30px'
+                }}>FRONT END</h2>
+                <ul style={
+                  {
+                    listStyle: 'none'
+                  }}>
+                  <li>react</li>
+                  <li>html</li>
+                  <li>css</li>
+                </ul>
+              </div>
+              <div className='back-end-stack' style={{ backgroundColor: 'red', flexGrow: 1 }}>
+                <h2>BACK END</h2>
+              </div>
+              <div className='tools' style={{ backgroundColor: 'silver', flexGrow: 1 }}>
+                <h2>TOOLS</h2>
+              </div>
+            </section>
+          </section>
+
+
+          <Link to="/page-2">Go back to the homepage</Link>
+        </Layout>
+
+      )
+    }} />
+
+
 )
 
 export default IndexPage
